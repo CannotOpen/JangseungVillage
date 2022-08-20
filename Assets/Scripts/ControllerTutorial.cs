f8604ff8dac675d7aaa4017c98c009b70b787f2a
Kim Hyeonwoo <1reds@naver.com>
2022-08-21 07:05:38 +0900

    Mod Tutorial Bug

--- Assets/Scripts/ControllerTutorial.cs
+++ Assets/Scripts/ControllerTutorial.cs
@@ -6,39 +6,67 @@ using UniRx;
 
 public class ControllerTutorial : MonoBehaviour
 {
+    [Header("Common")]
+    [SerializeField]
+    private Button closeButton;
+
+    #region First
+
+    [Header("First")]
     [SerializeField]
     private GameObject firstPage;
-    [SerializeField]
-    private GameObject normalModePage;
-    [SerializeField]
-    private GameObject speedModePage;
 
     [SerializeField]
     private Button explainNormalModeButton;
+
     [SerializeField]
     private Button explainSpeedModeButton;
 
-    // [SerializeField]
-    // private Button previousButton;
+    #endregion
 
-    // [SerializeField]
-    // private Button nextButton;
+    #region Normal
 
+    [Header("Normal")]
     [SerializeField]
     private List<GameObject> normalModePages;
+
+    [SerializeField]
+    private GameObject normalModePagePanel;
+
+    [SerializeField]
+    private Button previousButton;
+
+    [SerializeField]
+    private Button nextButton;
+
+    [SerializeField]
+    private Button goToSpeedModeButton;
+
+    #endregion
+
+    #region Speed Mode
+
+    [Header("Speed Mode")]
+    [SerializeField]
+    private GameObject speedModePagePanel;
+
+    [SerializeField]
+    private Button goToNormalModeButton;
+
+    #endregion
 
     private int nowIndex = 0;
 
     private void Start()
     {
-        // DeactiveButton(previousButton);
-        // ActiveButton(nextButton);
+        DeactiveButton(previousButton);
+        ActiveButton(nextButton);
 
         explainNormalModeButton.OnClickAsObservable()
             .Subscribe(_ =>
             {
                 firstPage.SetActive(false);
-                normalModePage.SetActive(true);
+                normalModePagePanel.SetActive(true);
             })
             .AddTo(gameObject);
 
@@ -46,60 +74,107 @@ public class ControllerTutorial : MonoBehaviour
             .Subscribe(_ =>
             {
                 firstPage.SetActive(false);
-                speedModePage.SetActive(true);
+                speedModePagePanel.SetActive(true);
             })
             .AddTo(gameObject);
 
-        // previousButton.OnClickAsObservable()
-        //     .Subscribe(_ =>
-        //     {
-        //         if (nowIndex <= 0)
-        //         {
-        //             return;
-        //         }
+        previousButton.OnClickAsObservable()
+            .Subscribe(_ =>
+            {
+                if (nowIndex <= 0)
+                {
+                    return;
+                }
 
-        //         normalModePages[nowIndex--].SetActive(false);
-        //         normalModePages[nowIndex].SetActive(true);
+                normalModePages[nowIndex--].SetActive(false);
+                normalModePages[nowIndex].SetActive(true);
 
-        //         if (nowIndex <= 0)
-        //         {
-        //             DeactiveButton(previousButton);
-        //         }
-        //         else
-        //         {
-        //             ActiveButton(nextButton);
-        //         }
-        //     })
-        //     .AddTo(gameObject);
+                if (nowIndex <= 0)
+                {
+                    DeactiveButton(previousButton);
+                }
+                else
+                {
+                    ActiveButton(previousButton);
+                    ActiveButton(nextButton);
+                }
+            })
+            .AddTo(gameObject);
 
-        // nextButton.OnClickAsObservable()
-        //     .Subscribe(_ =>
-        //     {
-        //         if (nowIndex >= normalModePages.Count - 1)
-        //         {
-        //             return;
-        //         }
+        nextButton.OnClickAsObservable()
+            .Subscribe(_ =>
+            {
+                if (nowIndex >= normalModePages.Count - 1)
+                {
+                    return;
+                }
 
-        //         normalModePages[nowIndex++].SetActive(false);
-        //         normalModePages[nowIndex].SetActive(true);
+                normalModePages[nowIndex++].SetActive(false);
+                normalModePages[nowIndex].SetActive(true);
 
-        //         if (nowIndex >= normalModePages.Count - 1)
-        //         {
-        //             DeactiveButton(nextButton);
-        //         }
-        //         else
-        //         {
-        //             ActiveButton(nextButton);
-        //         }
-        //     })
-        //     .AddTo(gameObject);
+                if (nowIndex >= normalModePages.Count - 1)
+                {
+                    DeactiveButton(nextButton);
+                }
+                else
+                {
+                    ActiveButton(previousButton);
+                    ActiveButton(nextButton);
+                }
+            })
+            .AddTo(gameObject);
+
+        closeButton.OnClickAsObservable()
+            .Subscribe(_ =>
+            {
+                gameObject.SetActive(false);
+            })
+            .AddTo(gameObject);
+
+        goToSpeedModeButton.OnClickAsObservable()
+            .Subscribe(_ =>
+            {
+                normalModePagePanel.SetActive(false);
+                speedModePagePanel.SetActive(true);
+            })
+            .AddTo(gameObject);
+
+        goToNormalModeButton.OnClickAsObservable()
+            .Subscribe(_ =>
+            {
+                normalModePagePanel.SetActive(true);
+                speedModePagePanel.SetActive(false);
+            })
+            .AddTo(gameObject);
+    }
+
+    private void OnEnable()
+    {
+        firstPage.SetActive(true);
+        normalModePagePanel.SetActive(false);
+        speedModePagePanel.SetActive(false);
+
+        nowIndex = 0;
+
+        DeactiveButton(previousButton);
+        ActiveButton(nextButton);
+    }
+
+    private void OnDisable()
+    {
+        normalModePages[0].SetActive(true);
+
+        for (int i = 1; i < normalModePages.Count; i++)
+        {
+            normalModePages[i].SetActive(false);
+        }
     }
 
     private void ActiveButton(Button button)
     {
         var imageColor = button.GetComponent<Image>().color;
         
-        imageColor = new Color(imageColor.r, imageColor.g, imageColor.b, 0.5f);
+        button.GetComponent<Image>().color = new Color(imageColor.r, imageColor.g, imageColor.b, 1f);
 
         button.enabled = true;
     }
@@ -108,7 +183,7 @@ public class ControllerTutorial : MonoBehaviour
     {
         var imageColor = button.GetComponent<Image>().color;
         
-        imageColor = new Color(imageColor.r, imageColor.g, imageColor.b, 0.5f);
+        button.GetComponent<Image>().color = new Color(imageColor.r, imageColor.g, imageColor.b, 0.5f);
 
         button.enabled = false;
     }
